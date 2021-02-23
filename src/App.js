a5bb805cffa6cb4ef3bf2ef8d765e1334db3cd54
Alexander <pashuk.sasha18@gmail.com>
2021-02-23 18:31:04 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from "react"
-import Statistics from "./components/Feedback/Feedback"
+import React from 'react';
+import StatisticsRender from './components/Feedback/Feedback';
 
-
-const App  = () => {
-  return ( 
+const App = () => {
+  return (
     <>
-     <Statistics good={0} neutral={0} bad={0}></Statistics>
- </>
- )
-}
-export default App
+      <StatisticsRender></StatisticsRender>
+    </>
+  );
+};
+export default App;
--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,43 +1,52 @@
-import React from "react";
-import s from "./Feedback.module.scss"
+import React from 'react';
+import s from './Feedback.module.scss';
+import FeedbackOptions from './FeedbackOptions';
+import Statistics from './Statistics';
 
-class Statistics extends React.Component {
-state = {
+class StatisticsRender extends React.Component {
+  state = {
     good: 0,
     neutral: 0,
-    bad: 0
+    bad: 0,
+  };
+
+  handleIncrement = e => {
+    let name = e.target.name;
+    this.setState(prevState => {
+      return {
+        [name]: prevState[name] + 1,
+      };
+    });
+  };
+
+  countTotalFeedback = () =>
+    this.state.good + this.state.bad + this.state.neutral;
+  countPositiveFeedbackPercentage = () =>
+    (this.state.good /
+      (this.state.good + this.state.bad + this.state.neutral)) *
+    100;
+
+  render() {
+    return (
+      <div className={s.statistics}>
+        <h2 className="title">Please leave feedback</h2>
+        <FeedbackOptions options={this.handleIncrement}></FeedbackOptions>
+        <h3 className="title">Statistics</h3>
+
+        {this.countTotalFeedback() === 0 ? (
+          <h3 className="title">No feedback given</h3>
+        ) : (
+          <Statistics
+            good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.countPositiveFeedbackPercentage()}
+          ></Statistics>
+        )}
+      </div>
+    );
   }
+}
 
-  handleIncrement =(e) => {
-      let name = e.target.name;
-      this.setState(prevState => {
-          return {
-            [name]: prevState[name] + 1
-          }
-      }
-        )
-  }
-
-     render (  ) {
-         return (
-            <div className={s.statistics}>
-                <h2 className="title">Please leave feedback</h2>
-                <button type="button" name='good' onClick={this.handleIncrement}>Good</button>
-                <button type="button" name='neutral' onClick={this.handleIncrement}>Neutral</button>
-                <button type="button" name='bad' onClick={this.handleIncrement}>Bad</button>
-                <div className="statistics">
-                <h2 className="title">Statistics</h2>
-                <span>Good {this.state.good}</span>
-                <span>Neutral {this.state.neutral}</span>
-                <span>Bad {this.state.bad}</span>
-                <span>Total {this.state.good +  this.state.bad + this.state.neutral}</span>
-                <span>Positive feedback: 100%</span>
-
-                </div>
-            </div>
-         )
-     }
- }
-
-
-export default Statistics;
+export default StatisticsRender;
